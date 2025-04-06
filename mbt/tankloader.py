065b7389b9ad8375d3475e107e9ab7558ac3a0b7
Indianojp <jp.asilva657@gmail.com>
2025-04-05 22:16:19 -0300

    Update tankloader.py
    added better ammo management and status reports

--- mbt/tankloader.py
+++ mbt/tankloader.py
@@ -1,28 +1,48 @@
 class Tankloader:
     def __init__(self,content: list):
         self.content = content
-        self.state = 0
-        self.loaded = None
-        self.rounds = {}
+        self.target = 0
+        self.ammo = False
+        self.chamber = "Empty"
+        self.racks = {}
         for i in range(0, len(content)):
-            self.rounds[i] = content[i]
+            self.racks[i] = content[i]
     def load(self, type:str):
+        if type not in self.racks.values():
+            return f"No {type} remaining!"
         shortest = len(self.content)
-        for i in self.rounds:
-            cwdist = (i - self.state) % len(self.content)
-            ccdist = (self.state - i) % len(self.content)
+        for i in self.racks:
+            cwdist = (i - self.target) % len(self.content)
+            ccdist = (self.target - i) % len(self.content)
             distance = min(cwdist,ccdist)
-            if self.rounds[i] == type and distance < shortest:
+            if self.racks[i] == type and distance < shortest:
                 shortest = i
-        self.state = shortest
-        if not self.loaded:
-            self.loaded = self.state
-            return self.rounds[self.loaded]+" loaded"
+        self.target = shortest
+        if self.chamber == "Empty":
+            self.chamber = (self.racks[self.target])
+            self.ammo = self.target
+            self.racks[self.target] = "Empty"
+            return self.chamber+" loaded"
+        else:
+            return f"{self.chamber} is loaded, {self.racks[self.target]} is next!"
+    def unload(self):
+        self.racks[self.ammo] = self.chamber
+        self.chamber = "Empty"
+        return f"{self.racks[self.ammo]} unloaded!"
     def shoot(self):
-        shot = self.rounds[self.loaded]
-        self.rounds[self.loaded] = None
-        return f"remaining shells: {self.rounds}"
+        if self.chamber == "Empty":
+            return "the chamber is empty"
+        shot = (self.chamber)
+        self.chamber = "Empty"
+        racks = set(self.racks.values())
+        racks.remove("Empty")
+        if self.target == self.ammo:
+            return f"{shot} shot! Remaining shells: {racks}"
+        else:
+            return f"{shot} shot! Remaining shells: {racks}\n{self.load(self.racks[self.target])}"
 
-m1 = Tankloader(["AP","HE","APHE","HEAT"])
+m1 = Tankloader(["AP","HE", "AP", "APHE","HEAT"])
 print(m1.load("HE"))
+print(m1.load("AP"))
 print(m1.shoot())
+print(m1.load("HE"))
