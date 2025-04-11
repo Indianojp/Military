334f4f296ef669f96558bc9d2d7c78f59c3f5246
Indianojp <jp.asilva657@gmail.com>
2025-04-11 09:37:35 -0300

    Update tankloader.py
    spacing for better readability

--- mbt/tankloader.py
+++ mbt/tankloader.py
@@ -1,4 +1,5 @@
 class Tankloader:
+
     def __init__(self,content: list):
         self.content = content
         self.target = 0
@@ -7,9 +8,12 @@ class Tankloader:
         self.racks = {}
         for i in range(0, len(content)):
             self.racks[i] = content[i]
+
     def load(self, type:str):
+
         if type not in self.racks.values():
             return f"No {type} remaining!"
+        
         shortest = len(self.content)
         for i in self.racks:
             cwdist = (i - self.target) % len(self.content)
@@ -18,6 +22,7 @@ class Tankloader:
             if self.racks[i] == type and distance < shortest:
                 shortest = i
         self.target = shortest
+
         if self.chamber == "Empty":
             self.chamber = (self.racks[self.target])
             self.ammo = self.target
@@ -25,17 +30,22 @@ class Tankloader:
             return self.chamber+" loaded"
         else:
             return f"{self.chamber} is loaded, {self.racks[self.target]} is next!"
+        
     def unload(self):
         self.racks[self.ammo] = self.chamber
         self.chamber = "Empty"
         return f"{self.racks[self.ammo]} unloaded!"
+    
     def shoot(self):
+
         if self.chamber == "Empty":
             return "the chamber is empty"
+        
         shot = (self.chamber)
         self.chamber = "Empty"
         racks = set(self.racks.values())
         racks.remove("Empty")
+
         if self.target == self.ammo:
             return f"{shot} shot! Remaining shells: {racks}"
         else:
